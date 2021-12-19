39bbf30044c3b1532eb42c5d36156a9a88da1c55
tangentsolution <tangentsolution@outlook.com>
2021-12-19 04:07:19 +0000

    Updated webapp.tf. Removed access key

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -3,8 +3,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    access_key           = "__storagekey__"
+    key                  = "terraform.tfstate"  
     
   }
 }
