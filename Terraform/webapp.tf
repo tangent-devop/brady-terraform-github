93aac50fb111cc943106fcf61f320bd88e27163b
tangentsolution <tangentsolution@outlook.com>
2021-12-21 16:31:13 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -8,6 +8,9 @@ terraform {
     
   }
 }
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "test" {
   name     = "TerraformBdy"
@@ -30,8 +33,5 @@ resource "azurerm_app_service" "test" {
   resource_group_name =    azurerm_resource_group.test.name
   app_service_plan_id =    azurerm_app_service_plan.test.id
 
-site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }  
+
 }
