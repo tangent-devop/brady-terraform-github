f1095c5aa56969804770b2e70fa4190119391bef
tangentsolution <tangentsolution@outlook.com>
2021-12-21 04:47:41 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 0.11"
-  features {}
   backend "azurerm" {
     storage_account_name = "terraformstorageaccount"
     container_name       = "terraform"
@@ -9,6 +8,7 @@ terraform {
     
   }
 }
+
 resource "azurerm_resource_group" "test" {
   name     = "TerraformBdy"
   location = "UK South"
@@ -29,4 +29,9 @@ resource "azurerm_app_service" "test" {
   location            =    azurerm_resource_group.test.location
   resource_group_name =    azurerm_resource_group.test.name
   app_service_plan_id =    azurerm_app_service_plan.test.id
+
+site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }  
 }
