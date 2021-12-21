8ea77bd7e58ac9c4bf15fef5bc9961c62fb8fe51
tangentsolution <tangentsolution@outlook.com>
2021-12-21 01:11:55 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.11"
+  required_version = ">= 0.15"
   backend "azurerm" {
     storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
