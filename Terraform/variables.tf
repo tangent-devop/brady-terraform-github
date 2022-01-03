e79ba3ce2e720d21f7517822354a0fae60da5f6d
tangentsolution <tangentsolution@outlook.com>
2022-01-03 03:12:03 +0000

    Added variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "azurerm_app_service_plan" {
+  default     = "bradyterraformplan"
+  description = "The name of the Web App Plan"
+}
+variable "azurerm_app_service" {
+  default     = "bradyterraformwebacc"
+  description = "The name of the Web App"
+}
