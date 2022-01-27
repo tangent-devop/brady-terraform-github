015fe29d85a6c267ca97756970f3d695ac5de19d
tangentsolution <tangentsolution@outlook.com>
2022-01-27 04:41:15 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -12,26 +12,26 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "dev" {
   name     = "TerraformBdy"
-  location = "UK South"
+  location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "asp-test" {
+resource "azurerm_app_service_plan" "dev" {
   name                = "__appserviceplan__"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+  location            = "${azurerm_resource_group.dev.location}"
+  resource_group_name = "${azurerm_resource_group.dev.name}"
 
   sku {
     tier = "Free"
     size = "F1"
   }
 }
-resource "azurerm_app_service" "as-test" {
-  name                = "__webappname1234__"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  app_service_plan_id = azurerm_app_service_plan.asp-test.id
+resource "azurerm_app_service" "dev" {
+  name                = "__appservicename__"
+  location            = "${azurerm_resource_group.dev.location}"
+  resource_group_name = "${azurerm_resource_group.dev.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
 }
 
 
