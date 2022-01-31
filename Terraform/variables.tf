0fc5d665e3302581ccfb2fc36d2a2daebc3093c0
tangentsolution <tangentsolution@outlook.com>
2022-01-31 02:03:40 +0000

    Updated variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,11 @@
+variable "resource-group-name" {
+  default     = "bdy-tf-resource-group"
+  description = "The prefix used for all resources in this example"
+}
+variable "location" {
+  default     = "West Europe"
+  description = "The Azure location where all resources in this example should be created"
+}
 variable "app-service-plan" {
   default     = "brady-tf-app-srv-plan"
   description = "The name of the Web App Plan"
