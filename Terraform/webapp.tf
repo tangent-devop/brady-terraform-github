7df1bffadb28c419783cdf3a43eaf1721f1f5181
tangentsolution <tangentsolution@outlook.com>
2022-01-31 00:57:26 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -28,7 +28,7 @@ resource "azurerm_app_service_plan" "dev" {
   }
 }
 resource "azurerm_app_service" "dev" {
-  name                = "__appservicename__"
+  name                = "${var.app-service-name}"
   location            = "${azurerm_resource_group.dev.location}"
   resource_group_name = "${azurerm_resource_group.dev.name}"
   app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
